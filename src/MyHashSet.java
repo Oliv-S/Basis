b371c832c9538dcda1df2abe2bc349642c57d8de
Oliv-S <36530788@users.noreply.github.com>
2018-06-19 18:57:49 +0200

    MyHashSet added

---
+++ src/MyHashSet.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+
+public class MyHashSet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		HashSet<String> myHashSet = new HashSet();
+		myHashSet.add("One");
+		myHashSet.add("Two");
+		myHashSet.add("Three");
+		myHashSet.add("Four");
+		myHashSet.add("Five");
+		
+		System.out.println(myHashSet);
+
+	}
+
+}
--- src/MyMap.java
+++ src/MyMap.java
@@ -7,6 +7,15 @@ public class MyMap {
 		// TODO Auto-generated method stub
 		Map<Integer,Integer> myMap = new HashMap<Integer,Integer>();
 		myMap.put(100, 200);
+		myMap.put(200, 100);
+		myMap.put(400, 500);
+		
+		//myMap.con
+		for (Integer key : myMap.keySet()) {
+			System.out.println("" + key + " - " + myMap.get(key));
+			
+		}
+		
 		
 
 	}
