18984f74339d5d24bf026e87ec7d423740850b7e
Oliv-S <36530788@users.noreply.github.com>
2018-06-19 18:24:44 +0200

    MyMap

--- src/MyMap.java
+++ src/MyMap.java
@@ -1,8 +1,13 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class MyMap {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Map<Integer,Integer> myMap = new HashMap<Integer,Integer>();
+		myMap.put(100, 200);
+		
 
 	}
 
